64374dfae790a95783755b3d4729addb126e932e
SireeshaKeesara <42659071+SireeshaKeesara@users.noreply.github.com>
2018-10-28 20:41:36 -0500

    icp10

--- icp_10/routes/index.js
+++ icp_10/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-
+var cors = require('cors');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var searchtext = req.query.searchtext;
